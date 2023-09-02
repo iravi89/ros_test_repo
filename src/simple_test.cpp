821a8d0fa73fce6e813cc62f1880eee55636a47f
Ravindra <42912207+KR-Ravindra@users.noreply.github.com>
2023-09-01 21:52:51 -0700

    Update simple_test.cpp

--- src/simple_test.cpp
+++ src/simple_test.cpp
@@ -4,16 +4,18 @@
 // bad function:
 // for example: how to deal with overflow?
 int add(int a, int b){
-    return a + b + 10;
+    return a + b;
 }
 
 TEST(NumberCmpTest, ShouldPass){
-    ASSERT_EQ(13, add(1,2));
+    ASSERT_EQ(3, add(1,2));
 }
 
+TEST(NumberCmpTest, ShouldFail){
+    ASSERT_EQ(INT_MAX, add(INT_MAX, 1));
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
