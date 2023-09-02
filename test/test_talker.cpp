a68b373e2b58aa6c7c8ec5a5c02f5dd7a57ed11a
iravi89 <ranjan.iravi89@gmail.com>
2023-09-02 15:07:02 +1000

    Update test_talker.cpp
    added prints.

--- test/test_talker.cpp
+++ test/test_talker.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <climits>
 #include "rostalker.h"
+#include <iostream>
 // bad function:
 // for example: how to deal with overflow?
 int add(int a, int b){
@@ -8,15 +9,18 @@ int add(int a, int b){
 }
 
 TEST(NumberCmpTest, ShouldPass){
+    std::cout<<"RUNNING TEST CASES...1"<<std::endl;
     ASSERT_EQ(3, add(1,2));
 }
 
 TEST(NumberCmpTest, ShouldFail){
+    std::cout<<"RUNNING TEST CASES...2"<<std::endl;
     ASSERT_EQ(INT_MAX, add(INT_MAX, 1));
 }
 
 TEST(RtTest, TalkerFunction){
     RosTalker rt;
+    std::cout<<"RUNNING TEST CASES...3"<<std::endl;
     ASSERT_EQ(3, rt.add(1,2));
 }
 
@@ -24,6 +28,7 @@ int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     // do not forget to init ros because this is also a node
     ros::init(argc, argv, "talker_tester");
+    std::cout<<"RUNNING TEST CASES..."<<std::endl;
     return RUN_ALL_TESTS();
 }
 
