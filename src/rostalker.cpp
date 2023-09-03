6803f33cdb6a28c217ce97e890276798b6f83251
ravi.ranjan <ravi.ranjan01@addverb.com>
2023-09-03 21:45:05 +1000

    changes in custome message,

--- src/rostalker.cpp
+++ src/rostalker.cpp
@@ -3,7 +3,7 @@
 void RosTalker::talk(){
     std_msgs::String msg;
     std::stringstream ss;
-    ss<<"hello world";
+    ss<<"hello world With new changes...";
     msg.data = ss.str();
     msg_pub.publish(msg);
 }
